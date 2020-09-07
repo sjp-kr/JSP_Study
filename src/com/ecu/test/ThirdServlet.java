5ab4d15274738fe36ff61def3852daf327b1e56d
sjp-kr <tony9803008@naver.com>
2020-09-07 10:10:16 +0900

    9/7

---
+++ src/com/ecu/test/ThirdServlet.java
@@ -0,0 +1,19 @@
+package com.ecu.test;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/third")
+public class ThirdServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.print("<h1>좋은 하루!</h1>");
+        out.close();
+    }
+}
