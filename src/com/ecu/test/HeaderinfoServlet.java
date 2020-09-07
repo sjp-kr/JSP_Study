ed272a2d4b7187df83b2500bc3a1b6a1f0210ec7
sjp-kr <tony9803008@naver.com>
2020-09-07 10:58:25 +0900

    HeaderInfoServlet

---
+++ src/com/ecu/test/HeaderinfoServlet.java
@@ -0,0 +1,29 @@
+package com.ecu.test;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+@WebServlet("/headerInfo")
+public class HeaderinfoServlet extends HttpServlet {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=EUC-KR");
+        PrintWriter out = resp.getWriter();
+        out.print("<html>");
+        out.print("<head><title>Request 정보 출력 Servlet</title></head>");
+        out.print("<body>");
+        out.print("<h3>요청 헤더 정보</h3>");
+        Enumeration<String> em = req.getHeaderNames();
+        while (em.hasMoreElements()) {
+            String s = em.nextElement();
+            out.println(s + " : " + req.getHeader(s) + "<br/>");
+        }
+        out.print("</body></html>");
+        out.close();
+    }
+}
