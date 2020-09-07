86faba3c032c606581e56ddc9412aa6e5b644755
sjp-kr <tony9803008@naver.com>
2020-09-07 11:20:02 +0900

    addInfo_Path/*

--- src/com/ecu/test/AdditionalInfoServlet.java
+++ src/com/ecu/test/AdditionalInfoServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/addInfo")
+@WebServlet("/addInfo/*")
 public class AdditionalInfoServlet extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html;charset=EUC-KR");
