e65cb59f5ffc16c6146ef583d4179e1b889ec492
sjp-kr <tony9803008@naver.com>
2020-09-08 15:15:51 +0900

    KoreanFilterXml

--- src/com/ecu/test/FlowFilterTwo.java
+++ src/com/ecu/test/FlowFilterTwo.java
@@ -4,11 +4,15 @@ import javax.servlet.*;
 
 public class FlowFilterTwo implements Filter {
 
+    String charset;
+
     public void init(FilterConfig config) {
         System.out.println("init() 호출 ......... two");
+        charset = config.getInitParameter("en");
     }
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws java.io.IOException, ServletException {
+        req.setCharacterEncoding(charset);
         System.out.println("doFilter() 호출 전 ........ two");
         chain.doFilter(req, resp);
         System.out.println("doFilter() 호출 후 ........ two");
