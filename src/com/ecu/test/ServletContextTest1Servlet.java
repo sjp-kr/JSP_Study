ba62a612685d6cb6d26c89398d5181b3dc0731e0
sjp-kr <tony9803008@naver.com>
2020-09-08 09:26:57 +0900

    ServletContext1

---
+++ src/com/ecu/test/ServletContextTest1Servlet.java
@@ -0,0 +1,30 @@
+package com.ecu.test;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/context1")
+public class ServletContextTest1Servlet extends HttpServlet {
+
+    ServletContext sc;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        sc = config.getServletContext();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.print("Context : " + sc);
+        out.close();
+    }
+}
